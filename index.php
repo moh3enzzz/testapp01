11c558fcbf94231531324b072b5df43889777743
moh3enzzz <39633671+moh3enzzz@users.noreply.github.com>
2021-01-06 13:55:29 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
-echo "welcome";
-echo "<br>";
-echo "The time is " . date("h:i:sa");
+echo "welcome"."<br>";
+echo "The time is " . date("h:i:sa")."<br>";
+
 date_default_timezone_set('Australia/Melbourne');
-echo "The time is " . date("h:i:sa");
-date_default_timezone_set('Asia/Qatar	');
-echo "The time is " . date("h:i:sa");
+echo "The time is " . date("h:i:sa")."<br>";
+
+date_default_timezone_set('Asia/Qatar');
+echo "The time is " . date("h:i:sa")."<br>";
 
 ?>
