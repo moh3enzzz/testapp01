4f19f89386d872d7202af7f71b3e77757429677f
moh3enzzz <39633671+moh3enzzz@users.noreply.github.com>
2021-01-06 11:52:45 +0330

    Create index.php

---
+++ policy/index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+
+echo "Policy <br>
+A policy is a deliberate system of principles to guide decisions and achieve rational outcomes. A policy is a statement of intent, and is implemented as a procedure or protocol. Policies are generally adopted by a governance body within an organization.";
+
+
+
+?>
